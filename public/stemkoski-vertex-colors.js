135c7f5e1f4dfc6ddc0d7dfd108d44c78ea4ed97
Gavin Ovsak <ovsak.gavin@gmail.com>
2014-01-29 12:40:03 -0500

    checker link fix

--- public/stemkoski-vertex-colors.js
+++ public/stemkoski-vertex-colors.js
@@ -14,7 +14,7 @@ app.drawFront = function(scene) {
     light.position.set(0,250,0);
     scene.add(light);
 
-    var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+    var floorTexture = new THREE.ImageUtils.loadTexture( 'http://stemkoski.github.io/Three.js/images/checkerboard.jpg' );
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
     floorTexture.repeat.set( 10, 10 );
     var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
